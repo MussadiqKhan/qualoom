580a9e5916ee5ad85d5cb2805dd4b7d8bd1b3ca6
Musadiq Khan <musadiq.khan@gmail.com>
2020-05-27 20:58:54 +0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,9 +1,13 @@
 var el = document.getElementById("menu")
+console.log("hi1")
 
 if (el) {
   document.getElementById("menu").addEventListener("click", addClass)
+  console.log("hi")
 }
 
 function addClass() {
+  console.log("hi2")
+
   document.getElementById("UL").classList.toggle("active")
 }
