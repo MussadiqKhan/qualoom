6bc173158841c1cfd42c9db4473b4214251bf7fd
Musadiq Khan <musadiq.khan@gmail.com>
2020-06-13 05:06:54 +0500

    interflora page

--- src/pages/caso-de-exito-interflora.js
+++ src/pages/caso-de-exito-interflora.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import interfloralogo from "../../static/images/logo-interflora.png"
+import ecommerce from "../../static/images/Interflora.003.platform.png"
 
 class Interflora extends Component {
   constructor(props) {
@@ -35,7 +36,7 @@ class Interflora extends Component {
             </div>
           </div>
         </div>
-        <div className="interflora-main">
+        <div className="interflora-section-1">
           <div>
             <img src={interfloralogo} alt="logo-interflora" />
             <p>
@@ -57,6 +58,107 @@ class Interflora extends Component {
               allowfullscreen
             ></iframe>
           </div>
+        </div>
+        <div className="interflora-section-2">
+          <h3>Objetivos del Proyecto</h3>
+          <p>
+            A comienzos de 2014, Interflora toma la decisión de migrar su
+            infraestructura de servidores hospedada en un hosting clásico a una
+            solución de cloud computing para tratar de paliar los numerosos
+            problemas que les suponía la administración de servidores, sobretodo
+            durante los periodos punta del negocio en los que se llega a
+            multiplicar por 5 su actividad habitual. Los principales objetivos
+            del proyecto son:
+          </p>
+          <ol>
+            <li>
+              Disponer de una infraestructura capaz de absorber la carga en los
+              periodos punta sin necesidad de mantener una flota de servidores
+              sobredimensionada.
+            </li>
+            <li>
+              Minimizar los costes operativos de la infraestructura de
+              servidores.
+            </li>
+            <li>
+              Optimizar los procesos de despliegue de las aplicaciones de
+              negocio.
+            </li>
+            <li>
+              Mejorar la infraestructura de monitorización de la plataforma.
+            </li>
+            <li>
+              Garantizar la seguridad en el acceso a las distintas aplicaciones
+              empresariales y minimizar la superficie de aplicación expuesta al
+              tráfico externo.
+            </li>
+            <li>
+              Optimizar los procesos de copias de seguridad y recuperación
+              frente a desastres.
+            </li>
+          </ol>
+          <p>
+            Para dar solución a todos estos requerimientos se decidió migrar
+            toda la infraestructura de servidores a Amazon Web Services,
+            adaptando cada parte del conjunto para maximizar los beneficios que
+            ofrecen los servicios gestionados de AWS.
+          </p>
+          <h3>E-Commerce</h3>
+          <p>
+            La plataforma de comercio electrónico de Interflora supone el
+            principal punto de entrada del negocio y es la parte más expuesta a
+            Internet, con lo que se hacen críticos los siguientes
+            requerimientos:
+          </p>
+          <ol>
+            <li>
+              Maximizar la disponibilidad del servicio, incluso en escenarios de
+              picos de carga inesperados o programados.
+            </li>
+            <li>Minimizar el tiempo de respuesta al cliente final</li>
+            <li>Minimizar la exposición de servicios en Internet</li>
+          </ol>
+          <p>
+            Para conseguir estos objetivos se optó por la implementación de
+            múltiples servicios gestionados de AWS:
+          </p>
+          <ol>
+            <li>
+              <b>EC2 + Autoscaling:</b> La implementación de Autoscaling permite
+              a Interflora adaptar el tamaño de su infraestructura a las
+              necesidades reales del negocio en cada momento e implantar un
+              mecanismo de recuperación automático en caso de fallo en alguno de
+              los nodos.
+            </li>
+            <li>
+              <b>Elastic Load Balancing (ELB):</b> El servicio de balanceador de
+              carga gestionado de Amazon permite minimizar la superficie de
+              ataque expuesta en Internet y proporciona un mecanismo de balanceo
+              de carga robusto y escalable con un tiempo de implantación de unos
+              pocos minutos.
+            </li>
+            <li>
+              <b>Relational Database Service (RDS):</b> Base de datos relacional
+              con gestión automatizada de copias de seguridad, replicación,
+              failover y escalado vertical.
+            </li>
+            <li>
+              <b>Elasticache:</b> Caché y almacén de sesiones compartido por
+              todos los nodos de aplicación.
+            </li>
+            <li>
+              <b>Cloudfront:</b> Red de distribución de contenidos con modalidad
+              de pago por uso para la optimización en la descarga de contenidos
+              estáticos y reducir la necesidad de aprovisionar una
+              infraestructura de servidores permanente para este fin.
+            </li>
+          </ol>
+          <img
+            src={ecommerce}
+            alt="ecommerce platform"
+            width="400"
+            style={{ margin: "0 auto" }}
+          />
         </div>
         <Footer />
       </div>
