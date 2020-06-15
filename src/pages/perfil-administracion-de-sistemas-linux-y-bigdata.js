da4434578d0fe0b9f13d1b61d474b5409f70d247
Musadiq Khan <musadiq.khan@gmail.com>
2020-06-15 20:45:17 +0500

    Update perfil-administracion-de-sistemas-linux-y-bigdata.js

--- src/pages/perfil-administracion-de-sistemas-linux-y-bigdata.js
+++ src/pages/perfil-administracion-de-sistemas-linux-y-bigdata.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import Header from "../components/header"
 
 class EmpleoForm extends Component {
   constructor(props) {
@@ -6,7 +7,35 @@ class EmpleoForm extends Component {
     this.state = {}
   }
   render() {
-    return <div></div>
+    return (
+      <div>
+        {" "}
+        <div
+          className="background-empleo"
+          style={{
+            background: `url(../images/cabecera-empleo.jpg)`,
+          }}
+        >
+          <div className="header_div">
+            <Header />
+          </div>
+          <div className="main">
+            <div className="empty"></div>
+            <div className="text">
+              <h1>Perfil Administración de</h1>
+              <h1>sistemas Línux y BigData</h1>
+              <p>
+                Qualoom busca incorporar a su equipo perfiles con experiencia
+                técnica en ecosistemas Linux y Big Data, tecnologías Spark,
+                Hadoop (Cloudera) con ganas de seguir aprendiendo y con
+                capacidad de desarrollar su carrera en una empresa estable y
+                puntura en nuevas tecnologías para importante cliente.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
