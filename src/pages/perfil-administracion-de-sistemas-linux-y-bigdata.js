7fa9cec4a76f2143bef2ae01366f0fd4ae38a220
Musadiq Khan <musadiq.khan@gmail.com>
2020-06-15 23:06:35 +0500

    update

--- src/pages/perfil-administracion-de-sistemas-linux-y-bigdata.js
+++ src/pages/perfil-administracion-de-sistemas-linux-y-bigdata.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import Header from "../components/header"
+import Footer from "../components/footer"
 
 class EmpleoForm extends Component {
   constructor(props) {
@@ -34,6 +35,39 @@ class EmpleoForm extends Component {
             </div>
           </div>
         </div>
+        <div className="job">
+          <h3>Descripción del puesto</h3>
+          <ol>
+            <li>
+              Ofrecemos la oportunidad de crecer profesionalmente en proyectos
+              innovadores y tecnológicamente complejos
+            </li>
+            <li>
+              El candidato formará parte del equipo que administra y opera los
+              sistemas de información de un importante cliente a nivel nacional
+            </li>
+            <li>
+              Desarrollará las tareas de administración de plataformas Linux,
+              Windows y sistemas de Bigdata Hadoop y Spark, aplicando la mejor
+              estrategia de gestión de sistemas y tratamiento de entornos de
+              grandes volúmenes de datos.
+            </li>
+            <li>
+              Además tendrá la oportunida de aprender y abordar infinidad de
+              restos tecnológicos a nivel formativo y profesional dado que al
+              ofrecer este servicio de operación se trabaja mano a mano con
+              multitud de áreas del cliente (ingenieros de datos, equipos de
+              automatización, capa de negocio)
+            </li>
+            <li>
+              Se realizará un seguimiento personalizado de su evolución,
+              atendiendo a nuevas necesidades tecnológicas del mercado y a
+              capacidades propias para adquirir y reforzar su conocimiento
+              permitiendo su crecimiento profesional y personal.
+            </li>
+          </ol>
+        </div>
+        <Footer />
       </div>
     )
   }
