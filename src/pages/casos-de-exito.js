a2fa06595f7fc4f80d3e8f76f87f9fbfff085116
Musadiq Khan <musadiq.khan@gmail.com>
2020-06-15 21:37:47 +0500

    Update casos-de-exito.js

--- src/pages/casos-de-exito.js
+++ src/pages/casos-de-exito.js
@@ -69,12 +69,12 @@ const Empleo = () => {
           </div>
         </div>
         <div className="casos-image" style={{ marginLeft: "50px" }}>
-          <Img fluid={data.image.nodes[4].childImageSharp.fluid} />
+          <Img fluid={data.image.nodes[5].childImageSharp.fluid} />
         </div>
       </div>
       <div className="casos-content-main reverse">
         <div className="casos-image" style={{ marginRight: "50px" }}>
-          <Img fluid={data.image.nodes[5].childImageSharp.fluid} />
+          <Img fluid={data.image.nodes[4].childImageSharp.fluid} />
         </div>
         <div className="casos-content casos-content-mobile">
           <div className="casos-logo">
@@ -113,7 +113,7 @@ const Empleo = () => {
           </div>
         </div>
         <div className="casos-image" style={{ marginLeft: "50px" }}>
-          <Img fluid={data.image.nodes[1].childImageSharp.fluid} />
+          <Img fluid={data.image.nodes[0].childImageSharp.fluid} />
         </div>
       </div>
       <Footer />
