2d038d1a7fdf92b92d9779a72864928fa603dc34
Musadiq Khan <musadiq.khan@gmail.com>
2020-06-16 22:56:28 +0500

    Update casos-de-exito.js

--- src/pages/casos-de-exito.js
+++ src/pages/casos-de-exito.js
@@ -58,7 +58,7 @@ const Empleo = () => {
       <div className="casos-content-main">
         <div className="casos-content">
           <div className="casos-logo">
-            <Img fluid={data.image.nodes[1].childImageSharp.fluid} />
+            <Img fluid={data.image.nodes[2].childImageSharp.fluid} />
           </div>
 
           <div className="casos-text">
@@ -77,16 +77,16 @@ const Empleo = () => {
           </div>
         </div>
         <div className="casos-image" style={{ marginLeft: "50px" }}>
-          <Img fluid={data.image.nodes[4].childImageSharp.fluid} />
+          <Img fluid={data.image.nodes[5].childImageSharp.fluid} />
         </div>
       </div>
       <div className="casos-content-main reverse">
         <div className="casos-image" style={{ marginRight: "50px" }}>
-          <Img fluid={data.image.nodes[3].childImageSharp.fluid} />
+          <Img fluid={data.image.nodes[4].childImageSharp.fluid} />
         </div>
         <div className="casos-content casos-content-mobile">
           <div className="casos-logo">
-            <Img fluid={data.image.nodes[0].childImageSharp.fluid} />
+            <Img fluid={data.image.nodes[1].childImageSharp.fluid} />
           </div>
 
           <div className="casos-text">
@@ -105,7 +105,7 @@ const Empleo = () => {
       <div className="casos-content-main">
         <div className="casos-content">
           <div className="casos-logo">
-            <Img fluid={data.image.nodes[2].childImageSharp.fluid} />
+            <Img fluid={data.image.nodes[3].childImageSharp.fluid} />
           </div>
 
           <div className="casos-text">
@@ -121,7 +121,7 @@ const Empleo = () => {
           </div>
         </div>
         <div className="casos-image" style={{ marginLeft: "50px" }}>
-          <Img fluid={data.image.nodes[5].childImageSharp.fluid} />
+          <Img fluid={data.image.nodes[0].childImageSharp.fluid} />
         </div>
       </div>
       <Footer />
