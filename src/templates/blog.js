1ee433db4ca0e78622fee7399c7688168ba45d50
Musadiq Khan <musadiq.khan@gmail.com>
2020-06-16 21:23:10 +0500

    Update blog.js

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -29,13 +29,7 @@ const Post = props => {
         const alt = node.data.target.fields.title["en-US"]
         const url =
           node.data.target.fields.file["en-US"].url + "?fm=jpg&fl=progressive"
-        return (
-          <img
-            alt={alt}
-            src={url}
-            style={{ width: "300px", height: "300px" }}
-          />
-        )
+        return <img alt={alt} src={url} style={{ width: "500px" }} />
       },
     },
   }
